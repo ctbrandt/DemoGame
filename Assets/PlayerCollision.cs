4ecb951a64fd115cac3df47d49f2c194c8821afb
Clayton Brandt <ctbrandt22@gmail.com>
2019-06-12 20:49:34 -0500

    Initial commit of working demo from Brackeys tutorial

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void OnCollisionEnter (Collision collisionInfo) {
+        if (collisionInfo.collider.tag == "Obstacle"){
+            Debug.Log(collisionInfo.collider.name);
+        }
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Reference to Rigidbody component called "rb"
+    public Rigidbody rb;
+    
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 1000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // We marked this as "fixed" update because
+    // we are using it to alter physics
+    void FixedUpdate()
+    {
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if ( Input.GetKey("d")) {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if ( Input.GetKey("a")) {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        /*
+        if ( Input.GetKey("w")) {
+            rb.AddForce(0, 0, 500 * Time.deltaTime);
+        }
+
+        if ( Input.GetKey("s")) {
+            rb.AddForce(0, 0, -500 * Time.deltaTime);
+        }
+         */
+    }
+}
